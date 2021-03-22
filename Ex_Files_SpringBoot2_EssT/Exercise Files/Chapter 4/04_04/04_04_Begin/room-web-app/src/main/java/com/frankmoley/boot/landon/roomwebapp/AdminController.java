864af19891601432bf66c6addada61e248380170
Cinthia <cinthia.tengan@gmail.com>
2021-03-22 10:56:57 -0300

    added security layer

---
+++ Ex_Files_SpringBoot2_EssT/Exercise Files/Chapter 4/04_04/04_04_Begin/room-web-app/src/main/java/com/frankmoley/boot/landon/roomwebapp/AdminController.java
@@ -0,0 +1,12 @@
+package com.frankmoley.boot.landon.roomwebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
+}
