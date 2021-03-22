b978021bcaf33d8bcd96600eb2d3ac8e11807159
Cinthia <cinthia.tengan@gmail.com>
2021-03-22 09:30:13 -0300

    update on files of exercise chapter 4 - begin

--- Ex_Files_SpringBoot2_EssT/Exercise Files/Chapter 4/04_02/04_02_Begin/room-web-app/src/main/java/com/frankmoley/boot/landon/roomwebapp/Room.java
+++ Ex_Files_SpringBoot2_EssT/Exercise Files/Chapter 4/04_02/04_02_Begin/room-web-app/src/main/java/com/frankmoley/boot/landon/roomwebapp/Room.java
@@ -1,9 +1,19 @@
 package com.frankmoley.boot.landon.roomwebapp;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="ROOM")
 public class Room {
+    @Id
+    @Column(name="ROOM_ID")
+    @GeneratedValue
     private long id;
+    @Column(name="NAME")
     private String name;
+    @Column(name="ROOM_NUMBER")
     private String number;
+    @Column(name="BED_INFO")
     private String info;
 
     public Room() {
